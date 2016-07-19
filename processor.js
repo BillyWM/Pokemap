48779e8f0b1df5b57efd0bf8a68cea36dff4e7af
Billy Wenge-Murphy <iamthebilly@gmail.com>
2016-07-18 20:43:52 -0700

    Map more Pokemon

--- processor.js
+++ processor.js
@@ -5,20 +5,25 @@ var lines = fs.readFileSync('pokemon.log').toString().split("\n");
 
 var poke, lat, lng, line;
 var parts = [];
+var name;
 
 var pokemon = [];
 
 for (var i=0; i < lines.length; i++) {
 	line = lines[i];
 
-	parts = line.match(/(\w+) is visible at \(([\d\.\-]+), ([\d\.\-]+)/);
+	parts = line.match(/([\w ]+) is visible at \(([\d\.\-]+), ([\d\.\-]+)/);
 	
 	if (parts === null) continue;
 	
 	//1,2,3 should be name, lat, lng
 	
+	name = parts[1];
+	name = name.toLowerCase();
+	name = name.trim();
+	
 	pokemon.push({
-		name: parts[1].toLowerCase(),
+		name: name,
 		lat: parseFloat(parts[2]),
 		lng: parseFloat(parts[3])
 	});
